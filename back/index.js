c6fd09ab24e456c6e074efd9a2d5c07c36573330
Ticy-lol <kilua2009@hotmail.es>
2021-02-07 21:47:02 -0300

    moongose

--- back/index.js
+++ back/index.js
@@ -2,33 +2,33 @@
 const express = require("express");
 const app = express();
 
-const mongoose = require('mongoose');
-const passport = require('passport');
-const session = require('express-session')
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
 
-
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
-var user_routes = require('./routes/userRoute');
+var user_routes = require("./routes/userRoute");
 app.use(session({ secret: "cats" }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use('/api', user_routes);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api", user_routes);
 
-
-mongoose.connect('mongodb+srv://ignaciollb:123@cluster0.gmmvc.mongodb.net/TesisGC?retryWrites=true&w=majority', (err, res) => {
-    
-if(err){
-        console.log("NO CONECTA")
+mongoose.connect(
+  "mongodb+srv://ignaciollb:123@cluster0.gmmvc.mongodb.net/TesisGC?retryWrites=true&w=majority",
+  (err, res) => {
+    if (err) {
+      console.log("NO CONECTA");
+    } else {
+      app.listen(9000, () => {
+        console.log("Esta corriendo en puerto 9000");
+      });
     }
-    else{
-        app.listen(9000, () => {
-
-            console.log("Esta corriendo en puerto 9000")
-        })
+  }
+);
 
 //comentario random sdasd
