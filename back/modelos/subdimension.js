f2a117ea9e5cd3f33ea80fe08a4d2176c30b4cb2
Ticy-lol <kilua2009@hotmail.es>
2021-02-07 21:50:59 -0300

    s
    s

--- back/modelos/subdimension.js
+++ back/modelos/subdimension.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const SubdimensionSchema = Schema({
   nombre_subdimension: String,
-  estandares: [{
-    descripcion: String,
-    preguntas: Number
-  }],
-  
+  estandares: [
+    {
+      descripcion: String,
+      preguntas: Number,
+    },
+  ],
 });
 
 module.exports = mongoose.model("user", SubdimensionSchema);
