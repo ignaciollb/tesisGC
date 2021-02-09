c9b9b5eeb6b46993a87042f3a7d268c2c2bdcf0b
ignaciollb <66446008+ignaciollb@users.noreply.github.com>
2021-02-09 15:16:03 -0300

    img login

--- front/src/Vistas/Login.js
+++ front/src/Vistas/Login.js
@@ -31,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundImage: 'url(https://www.microdata.es/wp-content/uploads/DesarrolloProyectos-2-1.jpg)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
