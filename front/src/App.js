0d7624dedb3277bc3b42ad06146a6c1189ed15fc
ignaciollb <66446008+ignaciollb@users.noreply.github.com>
2021-02-19 19:25:28 -0300

    Revert "Merge branch 'main' of https://github.com/ignaciollb/tesisGC into main"
    This reverts commit d123fcf72f12e7159d648b38ce74de76254bf8de, reversing
    changes made to 2f0201cfdd38009b7e57f54c0b68f1f2821fc2db.

--- front/src/App.js
+++ front/src/App.js
@@ -5,8 +5,7 @@ import Footer from "./Vistas/Footer";
 import Nav from "./Vistas/Nav";
 import Landing from "./Vistas/Landing";
 import Subdimension from "./Vistas/Subdimensiones";
-import Estandares from "./Vistas/Estandares_PGR";
-
+import Estandares from "./Vistas/Estandares";
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,25 +15,8 @@ import {
 } from "react-router-dom";
 import Login from './Vistas/Login.js'
 import Menu from './Vistas/Menu.js'
-import axios from 'axios';
-
-import { ProtectedRoute } from "./protectedRoute";
+import {ProtectedRoute} from './protectedRoute'
 function App() {
-
-  // function isLogued(view){
-  //   axios.
-  //     get("http://localhost:9000/api/usuarioInfo")
-  //     .then(
-  //       (response) =>{
-  //         if(response.data == "Tienes que hacer login"){
-  //           alert("Tienes que iniciar sesion")
-  //           return <Redirect to="/login"/>
-  //         }else{
-  //           return view
-  //         }
-  //       }
-  //     )
-  // }
 
   return (
     <Router>
@@ -43,8 +25,7 @@ function App() {
         <Route exact path="/login" component={Login}/>
         <ProtectedRoute exact path="/menu" component={Subdimension}/>
       </Switch>
-
-  </Router>
+    </Router>
     
   );
 }
