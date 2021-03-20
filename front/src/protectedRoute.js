2cd1e556ee55ae8b94b0e86e9f5760f02a3d6056
ignaciollb <66446008+ignaciollb@users.noreply.github.com>
2021-03-20 18:09:17 -0300

    redirect a login

--- front/src/protectedRoute.js
+++ front/src/protectedRoute.js
@@ -43,7 +43,7 @@ export const ProtectedRoute = ({
           return (
             <Redirect
               to={{
-                pathname: "/login",
+                pathname: "/",
                 state: {
                   from: props.location
                 }
